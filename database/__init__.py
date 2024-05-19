33a5dba999dd845d4ea36fdc31faab714ced0c9b
ViktorStankov <viktorstankov1@gmail.com>
2024-05-19 19:42:00 +0300

    refactor: Move tortoise definition into init file

--- database/__init__.py
+++ database/__init__.py
@@ -1,9 +1,19 @@
+from tortoise.contrib.quart import register_tortoise
+
+from application import config_helper
 from application.app_generator import app
-from .user_api import UserAPI
-from .connection_builder import init
 from .models import *
+from .user_api import UserAPI
 
-
-@app.before_serving
-async def before_serving():
-    await init()
+register_tortoise(
+    app=app,
+    db_url='mysql://{}:{}@{}:{}/{}'.format(
+        config_helper.get_config_value('database_username'),
+        config_helper.get_config_value('database_password'),
+        config_helper.get_config_value('database_url'),
+        config_helper.get_config_value('database_port'),
+        config_helper.get_config_value('database_schema'),
+    ),
+    modules={'models': ['database.models']},
+    generate_schemas=True
+)
