2394816d1b1f5ea9ba20a1899105a51a272b7852
ViktorStankov <viktorstankov1@gmail.com>
2024-06-01 23:37:22 +0300

    feat: Add frontend endpoints

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 import endpoints.authentication
-from application.app_generator import app
+from application import app
+from quart import send_from_directory
 
 app.register_blueprint(endpoints.authentication, url_prefix='/api')
+
+
+@app.route('/')
+@app.route('/login')
+@app.route('/register')
+async def root():
+    return await send_from_directory('./frontend/dist', 'index.html')
+
+
+@app.route('/<path:path>')
+async def assets(path):
+    return await send_from_directory('./frontend/dist', path)
+
 
 if __name__ == '__main__':
     app.run()
