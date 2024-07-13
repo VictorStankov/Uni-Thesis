9b7205e0f9770e9ec42803f9df731c0480c27496
ViktorStankov <viktorstankov1@gmail.com>
2024-07-13 13:40:29 +0300

    feat: Add interior endpoint to internal CarAPI

--- database/car_api.py
+++ database/car_api.py
@@ -1,4 +1,4 @@
-from database import Car, CarColour
+from database import Car, CarColour, CarInterior
 
 
 class CarAPI:
@@ -9,3 +9,7 @@ class CarAPI:
     @staticmethod
     async def get_car_colour(car_id: int):
         return await CarColour.filter(car_id=car_id).all()
+
+    @staticmethod
+    async def get_car_interior(car_id: int):
+        return await CarInterior.filter(car_id=car_id).all()
