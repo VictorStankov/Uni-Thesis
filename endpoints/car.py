6eabb802d3d5c3e442aff0a44bd05c6b86c8b1e8
ViktorStankov <viktorstankov1@gmail.com>
2024-07-13 13:40:44 +0300

    feat: Add interior endpoint to the external API

--- endpoints/car.py
+++ endpoints/car.py
@@ -14,3 +14,9 @@ async def get_cars():
 async def get_car_colours(car_id: int):
     colours = await CarAPI.get_car_colour(car_id)
     return {'car_colours': [car_colour.to_dict() for car_colour in colours]}
+
+
+@car_blueprint.route('/cars/<car_id>/interiors', methods=['GET'])
+async def get_car_interiors(car_id: int):
+    interiors = await CarAPI.get_car_interior(car_id)
+    return {'interiors': [interior.to_dict() for interior in interiors]}
