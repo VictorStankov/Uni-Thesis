0f848f6ceb1d3d1aa9b1cf97a963099d233fcec4
ViktorStankov <viktorstankov1@gmail.com>
2024-07-28 21:43:01 +0300

    feat: Order colours and interiors in API

--- database/car_api.py
+++ database/car_api.py
@@ -16,8 +16,8 @@ class CarAPI:
 
     @staticmethod
     async def get_car_colour(car_id: int):
-        return await CarColour.filter(car_id=car_id).all()
+        return await CarColour.filter(car_id=car_id).order_by("-is_base", "price_increase").all()
 
     @staticmethod
     async def get_car_interior(car_id: int):
-        return await CarInterior.filter(car_id=car_id).all()
+        return await CarInterior.filter(car_id=car_id).order_by("-is_base", "price_increase").all()
