feb69ccf5c8a3542c245c74fccfd9ce36e91e4e3
ViktorStankov <viktorstankov1@gmail.com>
2024-07-28 14:30:08 +0300

    feat: Implement a more robust backend routing

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 import endpoints
 from application import app
 from quart import send_from_directory
@@ -6,16 +8,15 @@ app.register_blueprint(endpoints.authentication, url_prefix='/api')
 app.register_blueprint(endpoints.car_blueprint, url_prefix='/api')
 
 
-@app.route('/')
-@app.route('/login')
-@app.route('/register')
-async def root():
-    return await send_from_directory('./frontend/dist', 'index.html')
-
-
+@app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
-async def assets(path):
-    return await send_from_directory('./frontend/dist', path)
+async def serve(path: str):
+    if path != "":
+        if os.path.exists('./frontend/dist' + '/' + path):
+            return await send_from_directory('./frontend/dist', path)
+        if path.split('/')[0] == 'img' and os.path.exists('./' + path):
+            return await send_from_directory('./', path, mimetype='image/png')
+    return await send_from_directory('./frontend/dist', 'index.html')
 
 
 if __name__ == '__main__':
