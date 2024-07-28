f4641eea85458ca3f57c6642f457812f77d7774e
ViktorStankov <viktorstankov1@gmail.com>
2024-07-28 17:06:35 +0300

    fix: Rename fields to fix weird name in database

--- database/models/CarInterior.py
+++ database/models/CarInterior.py
@@ -12,12 +12,12 @@ class Interior(Enum):
 
 class CarInterior(Model):
     id = fields.IntField(pk=True)
-    car_id = fields.ForeignKeyField('models.Car', related_name='interiors', related_query_name='interior')
+    car = fields.ForeignKeyField('models.Car', related_name='interiors', related_query_name='interior')
     interior_type = fields.data.CharEnumField(Interior)
     price_increase = fields.FloatField(null=False)
 
     def __str__(self):
-        return f"{self.car_id} - {self.interior_type.value} - {self.price_increase}"
+        return f"{self.car} - {self.interior_type.value} - {self.price_increase}"
 
     def to_dict(self):
         return {
