19305a0f1cf3cfcdc3262f73372e7d3be62dde9a
ViktorStankov <viktorstankov1@gmail.com>
2025-01-26 18:12:04 +0200

    fix: Change related name property of foreignkey fields to singular

--- database/models/Order.py
+++ database/models/Order.py
@@ -4,11 +4,11 @@ from tortoise.models import Model
 
 class Order(Model):
     id = fields.IntField(pk=True)
-    car = fields.ForeignKeyField('models.Car', related_name='orders', related_query_name='order')
-    interior_type = fields.ForeignKeyField('models.CarInterior', related_name='orders', related_query_name='order')
-    colour = fields.ForeignKeyField('models.CarColour', related_name='orders', related_query_name='order')
+    car = fields.ForeignKeyField('models.Car', related_name='order', related_query_name='order')
+    interior_type = fields.ForeignKeyField('models.CarInterior', related_name='order', related_query_name='order')
+    colour = fields.ForeignKeyField('models.CarColour', related_name='order', related_query_name='order')
     price = fields.FloatField(null=False)
-    order_placer = fields.ForeignKeyField('models.User', related_name='orders', related_query_name='order')
+    order_placer = fields.ForeignKeyField('models.User', related_name='order', related_query_name='order')
 
     def __str__(self):
         return f"{self.car} - {self.car} - {self.interior_type} - {self.colour} - {self.price} - {self.order_placer}"
