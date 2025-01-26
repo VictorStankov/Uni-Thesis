228ecb7c9d0965f4e2e801307ce037da3396022a
ViktorStankov <viktorstankov1@gmail.com>
2025-01-26 16:53:24 +0200

    fix: Simplify Login Page

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -50,53 +50,47 @@ export default function Login() {
     const button = !(usernameInput && passwordInput)
     return (
         <>
-            <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <img className="mx-auto h-20 w-auto"
-                         src={logo} alt="Atlas logo"/>
-                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in
+            <div className='p-10 flex-col justify-items-center w-1/4'>
+
+                <div>
+                    <img className='block mx-auto size-[11rem]'
+                         src={logo} alt="Atlas logo" />
+                    <h2 className='align-center text-lg font-bold md-10' >Sign in
                         to your account</h2>
                 </div>
 
-                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form className="space-y-6" action="#">
+                <div className='mt-10 w-full'>
+                    <form>
                         <div>
-                            <label htmlFor="email"
-                                   className="block text-sm font-medium leading-6 text-gray-900">Username</label>
-                            <div className="mt-2">
-                                <input id="username" name="username" autoComplete="email" required inputMode="text"
-                                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                       onChange={handleUsernameChange} value={username}
-                                />
+                            <label htmlFor="username">Username</label>
+                            <div className="my-1">
+                                <input id="username" name="username" required
+                                       className='w-full p-2 border border-solid border-gray-300'
+                                       onChange={handleUsernameChange} value={username} />
+                            </div>
+
+                        <div className="my-3">
+                            <label htmlFor="password">Password</label>
+                            <div className="my-1">
+                                <input id="password" name="password" type="password" required
+                                       className='w-full p-2 border border-solid border-gray-300'
+                                       onChange={handlePasswordChange} value={password} />
                             </div>
                         </div>
 
-                        <div>
-                            <label htmlFor="password"
-                                   className="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                            <div className="mt-2">
-                                <input id="password" name="password" type="password" autoComplete="current-password"
-                                       required
-                                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                       onChange={handlePasswordChange} value={password}
-                                />
-                            </div>
-                        </div>
-
-                        <div>
+                        <div className="mt-10">
                             <button type="submit" onClick={onSubmitClick} disabled={button}
-                                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
-                                in
-                            </button>
+                                    className='w-full p-2.5 bg-indigo-600 hover:bg-indigo-500 text-white cursor-pointer font-semibold'>
+                                Sign in</button>
+                        </div>
                         </div>
                     </form>
 
-                    <p className="mt-10 text-center text-sm text-gray-500 w-64">
-                        Not a member?
-                        <a href="/register"
-                           className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"> Sign Up Here</a>
+                    <p className='mt-4 text-center'>
+                        <span>Not a member? </span>
+                        <a href="/register" className='text-indigo-600 hover:text-indigo-500 font-semibold'>Sign Up Here</a>
                     </p>
-                    <p className="mt-10 text-center text-sm text-red-500 w-64">{message}</p>
+                    <p className='mt-4 text-center text-red-500 text-wrap'>{message}</p>
                 </div>
             </div>
         </>
