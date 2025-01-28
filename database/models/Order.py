1f49fbac89255a6ccde98c687b6ccd588999122e
ViktorStankov <viktorstankov1@gmail.com>
2025-01-28 23:14:24 +0200

    fix: Improve naming for database field

--- database/models/Order.py
+++ database/models/Order.py
@@ -5,19 +5,19 @@ from tortoise.models import Model
 class Order(Model):
     id = fields.IntField(pk=True)
     car = fields.ForeignKeyField('models.Car', related_name='order', related_query_name='order')
-    interior_type = fields.ForeignKeyField('models.CarInterior', related_name='order', related_query_name='order')
+    interior = fields.ForeignKeyField('models.CarInterior', related_name='order', related_query_name='order')
     colour = fields.ForeignKeyField('models.CarColour', related_name='order', related_query_name='order')
     price = fields.FloatField(null=False)
     order_placer = fields.ForeignKeyField('models.User', related_name='order', related_query_name='order')
 
     def __str__(self):
-        return f"{self.car} - {self.car} - {self.interior_type} - {self.colour} - {self.price} - {self.order_placer}"
+        return f"{self.car} - {self.car} - {self.interior} - {self.colour} - {self.price} - {self.order_placer}"
 
     async def to_dict(self):
         return {
             'id': self.id,
             'car': (await self.car).to_dict(),
-            'interior_type': (await self.interior_type).to_dict(),
+            'interior': (await self.interior).to_dict(),
             'colour': (await self.colour).to_dict(),
             'price': self.price
         }
