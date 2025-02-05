0de39d0304ce3e7f42b526b0f6024e6b5ac402f5
ViktorStankov <viktorstankov1@gmail.com>
2025-02-05 22:28:47 +0200

    feat: Add Status and created/updated timestamp fields

--- database/models/Order.py
+++ database/models/Order.py
@@ -1,6 +1,15 @@
+from datetime import datetime, UTC
+from enum import Enum
+
 from tortoise import fields
 from tortoise.models import Model
 
+class Status(Enum):
+    Ordered = 'Ordered'
+    Paid = 'Paid'
+    Manufactured = 'Manufactured'
+    Shipped = 'Shipped'
+    Completed = 'Completed'
 
 class Order(Model):
     id = fields.IntField(pk=True)
@@ -9,6 +18,9 @@ class Order(Model):
     colour = fields.ForeignKeyField('models.CarColour', related_name='order', related_query_name='order')
     price = fields.FloatField(null=False)
     order_placer = fields.ForeignKeyField('models.User', related_name='order', related_query_name='order')
+    created_at = fields.DatetimeField(null=False, auto_now_add=True)
+    updated_at = fields.DatetimeField(null=False, auto_now=True)
+    status = fields.data.CharEnumField(Status, null=False, default=Status.Ordered)
 
     def __str__(self):
         return f"{self.car} - {self.car} - {self.interior} - {self.colour} - {self.price} - {self.order_placer}"
