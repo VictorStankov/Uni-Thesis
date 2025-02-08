4f3053455a653a1e4d399b7b720f3ffa61e93190
ViktorStankov <viktorstankov1@gmail.com>
2025-02-08 19:29:05 +0200

    feat: Add Employee table

---
+++ database/models/Employee.py
@@ -0,0 +1,24 @@
+from tortoise import fields
+from tortoise.models import Model
+
+
+class Employee(Model):
+    id = fields.IntField(pk=True)
+    user = fields.ForeignKeyField('models.User')
+    position = fields.ForeignKeyField('models.EmployeePosition')
+    manager = fields.ForeignKeyField('models.Employee', related_name='employees', related_query_name='employees', null=True)
+
+    def __str__(self):
+        return f'{self.id} - {self.user} - {self.position} - {self.position}'
+
+    async def to_dict(self):
+        return {
+            'id': self.id,
+            'user': (await self.user).username,
+            'position': (await self.position).to_dict(),
+            'manager': (await self.manager).to_dict() if self.manager else {},
+        }
+
+
+    class Meta:
+        table = 'employee'
