714b52e3dbd72d320c8707d6005a711cec70a9a1
ViktorStankov <viktorstankov1@gmail.com>
2025-03-01 23:55:08 +0200

    Feat: Add support for employee order retrieval

--- endpoints/order.py
+++ endpoints/order.py
@@ -1,9 +1,9 @@
 from marshmallow import ValidationError
 from quart import Blueprint, request
 
-from database import User
+from database import User, Employee
 from database import OrderAPI, CarAPI
-from .helpers import login_required
+from .helpers import login_required, employee_login_required
 
 from .models import OrderSchema
 from application.exceptions import NoEmployeesFoundOrderException
@@ -17,8 +17,9 @@ async def get_order_by_id(user: User, order_id: int):
         return {'message': f'No order found for ID {order_id}'}, 404
 
     order = await OrderAPI.get_order(order_id)
+    employee = await Employee.get_or_none(user=user)
 
-    if await order.order_placer != user:
+    if await order.order_placer != user and await order.employee != employee:
         return {'message': f'You are not authorized to view order {order_id}'}, 403
 
     return await order.to_dict(), 200
@@ -27,6 +28,21 @@ async def get_order_by_id(user: User, order_id: int):
 @login_required
 async def get_user_orders(user: User):
     orders = await OrderAPI.get_user_orders(user.id)
+    return {
+        'orders': [
+            {
+                'id': order.id,
+                'car': (await order.car).to_dict(),
+                'status': (await order.status).to_dict()
+            } for order in orders
+        ]
+    }, 200  # FIXME
+
+
+@order_blueprint.route('/employee/orders', methods=['GET'])
+@employee_login_required
+async def get_employee_orders(employee: Employee):
+    orders = await OrderAPI.get_employee_orders(employee.id)
     return {
         'orders': [
             {
