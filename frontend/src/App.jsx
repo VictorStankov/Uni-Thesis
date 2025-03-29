193c35e7cd39889b5789e06c91855d7ec724ed37
ViktorStankov <viktorstankov1@gmail.com>
2025-03-29 16:13:38 +0200

    chore: Fix route paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Catalogue from "./routes/Catalogue.jsx";
 import EmployeeTasksList from "./routes/EmployeeTasks.jsx";
 import OrdersList from "./routes/Orders.jsx";
 import EmployeeOrderDetails from "./routes/components/EmployeeTasks/EmployeeOrderDetails.jsx";
+import EmployeeTestDriveDetails from "./routes/components/EmployeeTasks/EmployeeTestDriveDetails.jsx";
 
 export default function App() {
     return (
@@ -17,8 +18,9 @@ export default function App() {
                 <Route path="/register" element={<Register/>}/>
                 <Route path="/catalogue" element={<Catalogue/>}/>
                 <Route path="/catalogue/:itemName" element={<Catalogue/>}/>
-                <Route path="/employee_orders" element={<EmployeeTasksList/>}/>
+                <Route path="/employee_tasks" element={<EmployeeTasksList/>}/>
                 <Route path="/employee_order/:id" element={<EmployeeOrderDetails/>}/>
+                <Route path="/employee_test_drive/:id" element={<EmployeeTestDriveDetails/>}/>
                 <Route path="/orders" element={<OrdersList/>}/>
                 <Route path="/order/:id" element={<OrdersList/>}/>
                 <Route path="*" element={<Home/>}/>
@@ -50,7 +52,7 @@ function Layout() {
                             loggedIn ?
                                 is_employee ?
                                     <li className="px-2">
-                                        <Link to="/employee_orders">Assigned Orders</Link>
+                                        <Link to="/employee_tasks">Assigned Tasks</Link>
                                     </li> :
                                     <li className="px-2">
                                         <Link to="/orders">Orders</Link>
