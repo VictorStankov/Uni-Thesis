c0cd7df1cc6836f78678e969dd6ef53eac996322
ViktorStankov <viktorstankov1@gmail.com>
2025-03-29 15:50:53 +0200

    feat: Add Employee Test Drive Details component

---
+++ frontend/src/routes/components/EmployeeTasks/EmployeeTestDriveDetails.jsx
@@ -0,0 +1,47 @@
+import {useEffect, useState} from "react";
+import {authFetch} from "../../../auth.jsx";
+import {useNavigate, useParams} from "react-router-dom";
+
+export default function EmployeeTestDriveDetails() {
+    const navigate = useNavigate()
+    const { id } = useParams();
+
+    const [response, setResponse] = useState({})
+
+    useEffect(() => {
+            authFetch(`/api/employee/test_drive/${id}`, {
+                method: 'get'
+            })
+                .then(r => {
+                    if (r.ok)
+                        return r.json()
+                    throw new Error(r.statusText)
+                })
+                .then(data => {
+                    setResponse(data);
+                })
+                .catch(reason => {
+                    console.log(reason)
+                    navigate('/employee_orders')
+                })
+        }, []);
+
+    console.log(response)
+
+    return(
+        <div className='flex-row flex flex-nowrap'>
+            <div className=''>
+                <img className="w-full rounded-lg" src={'/img/' + response?.car?.base_image_path} alt="Picture of a car"/>
+            </div>
+            <div className='flex flex-col'>
+                <p>{response?.car?.name}</p>
+                <p>{response?.status?.name}</p>
+                <p>{response?.user?.first_name} {response?.user?.last_name}</p>
+                <div className='flex flex-row'>
+                    <a className='px-2' href={'mailto:' + response?.user?.email}>Email</a>
+                    <a className='px-2' href={'tel:' + response?.user?.phone}>Call</a>
+                </div>
+            </div>
+        </div>
+    )
+}
