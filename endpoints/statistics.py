cc53a51ae1609249a3f8949a7b77238e54720c50
ViktorStankov <viktorstankov1@gmail.com>
2025-04-06 22:21:19 +0300

    feat: Add endpoint for employee statistics

---
+++ endpoints/statistics.py
@@ -0,0 +1,22 @@
+from quart import Blueprint
+
+from database import Employee, OrderAPI
+from endpoints.helpers.authentication import employee_login_required
+
+statistics_blueprint = Blueprint('statistics', __name__)
+
+@statistics_blueprint.route('/employee_order_statistics', methods=['GET'])
+@employee_login_required
+async def get_employee_order_statistics(employee: Employee):
+    orders = await OrderAPI.get_employee_orders(employee.id)
+
+    results = {}
+
+    for order in orders:
+        status = await order.status
+        if status.name not in results.keys():
+            results[status.name] = 1
+        else:
+            results[status.name] += 1
+
+    return [{'label': k, 'value': v} for k, v in results.items()]
