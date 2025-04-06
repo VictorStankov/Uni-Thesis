16f2ec1aa1cc25cb3a29fe2dfe43936c7339914e
ViktorStankov <viktorstankov1@gmail.com>
2025-04-06 22:24:14 +0300

    feat: Add EmployeeStatistics component

---
+++ frontend/src/routes/components/Statistics/EmployeeStatistics.jsx
@@ -0,0 +1,36 @@
+import { PieChart } from '@mui/x-charts/PieChart';
+import {authFetch} from "../../../auth.jsx";
+import {useEffect, useState} from "react";
+
+export default function EmployeeStatistics() {
+    const [response, setResponse] = useState([])
+
+    useEffect(() => {
+        authFetch(`/api/employee_order_statistics`, {
+            method: 'get'
+        })
+            .then(r => {
+                if (r.ok)
+                    return r.json()
+                throw new Error(r.statusText)
+            })
+            .then(data => {
+                setResponse(data);
+            })
+            .catch(reason => {
+                console.log(reason)
+            })
+        }, []);
+
+    return(
+        <PieChart
+            series={[{
+                data: response,
+                highlightScope: { fade: 'global', highlight: 'item' },
+                faded: { innerRadius: 15, additionalRadius: -15},
+            }]}
+            width={400}
+            height={200}
+        />
+    )
+}
