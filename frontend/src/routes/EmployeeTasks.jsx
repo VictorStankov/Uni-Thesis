79037a4e791673bf0dfc34463e312635e0bfcb59
VictorStankov <viktorstankov1@gmail.com>
2025-07-23 22:50:18 +0300

    feat: Rework employee task main page

--- frontend/src/routes/EmployeeTasks.jsx
+++ frontend/src/routes/EmployeeTasks.jsx
@@ -2,11 +2,10 @@ import EmployeeOrderList from "./components/EmployeeTasks/Orders/EmployeeOrderLi
 import EmployeeTestDriveList from "./components/EmployeeTasks/TestDrives/EmployeeTestDriveList.jsx";
 
 export default function EmployeeTasksList() {
-
     return (
-        <div className='flex flex-col justify-center'>
-            <h1 className='text-center font-bold text-2xl'>Assigned Tasks</h1>
-            <div className='grid grid-cols-2'>
+        <div className="px-6 py-10 max-w-6xl mx-auto w-full">
+            <h1 className="text-3xl font-bold text-center mb-10">Assigned Tasks</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                 <EmployeeOrderList/>
                 <EmployeeTestDriveList/>
             </div>
