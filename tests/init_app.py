545e80deb212188d313dd412935daefe7c62939b
VictorStankov <viktorstankov1@gmail.com>
2025-08-29 02:42:37 +0300

    feat: Add API tests

---
+++ tests/init_app.py
@@ -0,0 +1,26 @@
+import pytest_asyncio
+
+from app import app as _app
+
+
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def app():
+    app = _app
+
+    app.config.update({
+        "TESTING": True,
+    })
+
+    return app
+
+    # clean up / reset resources here
+
+
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def client(app):
+    return app.test_client()
+
+
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def runner(app):
+    return app.test_cli_runner()
