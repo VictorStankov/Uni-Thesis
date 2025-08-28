5832c389badd1c582e92a51c20f2bbd7f4fb08d1
VictorStankov <viktorstankov1@gmail.com>
2025-08-29 02:41:36 +0300

    fix: Add enums to init imports

--- database/models/__init__.py
+++ database/models/__init__.py
@@ -1,11 +1,11 @@
 from .UserBasicInfo import UserBasicInfo
 from .User import User
-from .Car import Car
-from .CarColour import CarColour
-from .CarInterior import CarInterior
+from .Car import Car, CarType
+from .CarColour import CarColour, Colour
+from .CarInterior import CarInterior, Interior
 from .OrderStatus import OrderStatus
 from .Employee import Employee
-from .EmployeePosition import EmployeePosition
+from .EmployeePosition import EmployeePosition, Position
 from .Order import Order
 from .TestDriveStatus import TestDriveStatus
 from .TestDrive import TestDrive
