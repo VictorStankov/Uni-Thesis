3fa5f3d111b656fbd857558c7daa09022bcba1ff
VictorStankov <viktorstankov1@gmail.com>
2025-08-30 18:01:26 +0300

    fix: Make logout button cursor to pointer

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -48,7 +48,7 @@ export default function Navbar() {
                         {loggedIn ? (
                             <button
                                 onClick={logoutAndRedirect}
-                                className="text-red-600 hover:text-red-800 font-medium"
+                                className="text-red-600 hover:text-red-800 font-medium cursor-pointer"
                             >
                                 Log Out
                             </button>
